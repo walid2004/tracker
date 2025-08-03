cab9ea5f1a0de283427316bf9893476eaedaa37f
waled <lodaragab@gmail.com>
2025-08-03 23:02:17 +0200

    progress: 1

--- v.py
+++ v.py
@@ -1,12 +1,35 @@
 import json
-import tkinter
+import tkinter as tk
 import time
 
 
-def update(x):
+def add(x):
     with open('w+','history.json') as f:
         datas = json.load(f)
-        datas
+        datas.append(x)
+        json.dump(datas,f)
 
-    pass
-root = tkinter.Tk()
+def accessor():
+    with open('w+','history.json') as f:
+        datas = json.load(f)
+        return datas
+    
+root = tk.Tk()
+root.title('J.O.B')
+
+naming = tk.Entry(root,width=40)
+naming.pack()
+
+position = tk.Entry(root)
+position.pack
+
+status = tk.Entry(root)
+status.pack()
+
+search_button = tk.Button(root, text='Search')
+search_button.pack()
+
+add_button = tk.Button(root, text='Add')
+add_button.pack()
+
+root.mainloop()
